d97671996db0098d8cae420a652f8fd807fa2da7
Jake Nocentino <jrnocentino@gmail.com>
2019-04-05 00:41:48 -0400

    Added new classes and started work on them... however, it is late and I am tired. Until tomorrow (later today)

---
+++ OrderedList.h
@@ -0,0 +1,23 @@
+#ifndef ORDEREDLIST_H
+#define ORDEREDLIST_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+namespace orderedlist
+{
+    class OrderedList
+    {
+    public:
+        // "Gang of Three"
+        OrderedList();
+        OrderedList(const OrderedList& orig);
+        ~OrderedList();
+    private:
+
+    };
+}
+
+#endif /* ORDEREDLIST_H */
+
